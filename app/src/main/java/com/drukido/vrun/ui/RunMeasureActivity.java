fde5b1fd637cb7b106d4105b9f85e593bf2a2a7e
OmerDrukman <omerdru@gmail.com>
2015-11-07 14:53:19 +0200

    more

--- app/src/main/java/com/drukido/vrun/ui/RunMeasureActivity.java
+++ app/src/main/java/com/drukido/vrun/ui/RunMeasureActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.drukido.vrun.R;
+import com.drukido.vrun.ui.views.MyTimePickerDialog;
+import com.drukido.vrun.ui.views.TimePicker;
+
+import java.util.Calendar;
 
 public class RunMeasureActivity extends AppCompatActivity {
 
@@ -15,17 +19,28 @@ public class RunMeasureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_run_measure);
 
         // Run Duration Edit Text
-        EditText duration = (EditText) findViewById(R.id.runMeasure_etxt_Duration);
+        final EditText duration = (EditText) findViewById(R.id.runMeasure_etxt_Duration);
         duration.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                createDurationDialog();
+            public void onClick(final View v) {
+                showPicker((EditText)v);
             }
         });
 
     }
 
-    protected void createDurationDialog() {
+    public void showPicker(final EditText v){
+        Calendar now = Calendar.getInstance();
+        MyTimePickerDialog mTimePicker = new MyTimePickerDialog(this, new MyTimePickerDialog.OnTimeSetListener() {
 
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute, int seconds) {
+                // TODO Auto-generated method stub
+                v.setText("Time" + String.format("%02d", hourOfDay) +
+                        ":" + String.format("%02d", minute) +
+                        ":" + String.format("%02d", seconds));
+            }
+        }, now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), now.get(Calendar.SECOND), true);
+        mTimePicker.show();
     }
 }
